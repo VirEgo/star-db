b3d7657968b84313d841c540d1475a2e2f790037
VirEgo <linx123554@gmail.com>
2021-01-11 15:35:04 +0200

    sync swapi service

--- src/index.js
+++ src/index.js
@@ -1,19 +1,1 @@
 
-// https://swapi.dev
-
-const getResource = async (url) => {
-  const res = await fetch(url);
-  if (!res.ok) {
-    throw new Error(`Could not fetch ${url}, receive ${res.status}`)
-  }
-  const body = await res.json();
-  return body;
-}
-
-getResource('https://swapi.dev/api/people/1/')
-  .then((body) => {
-    console.log(body)
-  })
-  .catch((err) => {
-    console.error(err);
-  });
